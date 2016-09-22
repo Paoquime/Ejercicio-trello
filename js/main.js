211315cfc6c2f7966b396c0b4eaf9055293db039
Paoquime <paola.qmelendez@gmail.com>
2016-09-22 16:13:56 -0500

    agregando cambios css, versión 1 terminada

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 window.addEventListener("load", function() {
 	var notaUno = document.getElementById("nota-inicial");
 	var notaDos = document.getElementById("nota");
-	var boton = document.getElementById("boton")
+	var boton = document.getElementById("boton");
+	var fondo = document.getElementById("fondo");
 
 	notaUno.addEventListener("click", function(e){
 		mostrarInput(notaDos);
 	});
 
 	function mostrarInput(notaDos){
+		notaUno.style.display = "none";
 		notaDos.style.display = "block";
 		boton.style.display =  "block";
+		fondo.style.display = "block";
 	}
-
 });
