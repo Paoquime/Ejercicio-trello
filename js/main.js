0c9fe19b204bfa4b48fd0eea7763cf2aea644eb4
Paoquime <paola.qmelendez@gmail.com>
2016-09-22 12:04:44 -0500

    agregando v0.0.1

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 window.addEventListener("load", function() {
 	var notaUno = document.getElementById("nota-inicial");
 	var notaDos = document.getElementById("nota");
+	var boton = document.getElementById("boton")
 
-	notaUno.addEventListener("click", function(){
+	notaUno.addEventListener("click", function(e){
 		mostrarInput(notaDos);
 	});
 
 	function mostrarInput(notaDos){
-		notaDos.style = "block";
+		notaDos.style.display = "block";
+		boton.style.display =  "block";
 	}
 
 });
