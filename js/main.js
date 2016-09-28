2d0f0708368473515f670980ff53c2f7c0acc1db
Paoquime <paola.qmelendez@gmail.com>
2016-09-27 21:25:20 -0500

    agregando versión 3

--- js/main.js
+++ js/main.js
@@ -1,30 +1,29 @@
 window.addEventListener("load", function() {
-	var inicial = document.getElementById("inicial");
+	var notaUno = document.getElementById("inicial");
 	var notaDos = document.getElementById("nota");
 	var boton = document.getElementById("boton");
 	var fondo = document.getElementById("fondo");
 	
 
-	inicial.addEventListener("click", function(){
+	notaUno.addEventListener("click", function(){
 		mostrarInput();
 
-		inicial.style.display = "none";
+		notaUno.style.display = "none";
 
     });
-
 
 	boton.addEventListener("click", function(e) {
 		e.preventDefault();
 		agregarTitulo(notaDos, this);
 		fondo.style.display = "none";
+		agregarDiv();
 	});
-
 
 	function mostrarInput(){
 		fondo.style.display ="block";
 	}
 
-	function agregarTitulo(notaDos, inicial){
+	function agregarTitulo(notaDos, notaUno){
 		var div = document.createElement("div");
 		var btn = document.createElement("button");
 
@@ -35,12 +34,25 @@ window.addEventListener("load", function() {
 		div.innerHTML = notaDos.value;
 		btn.textContent = "Añadir una tarjeta...";
 
-		var insertar = inicial.parentElement.parentElement;
+		var insertar = notaUno.parentElement.parentElement;
 		insertar.insertBefore(btn, insertar.childNodes[0]);
 		insertar.insertBefore(div, insertar.childNodes[0]);
 		notaDos.value="";
 	}
 
+	function agregarDiv(){
+		var newNota = document.createElement("div");
+		contenedor.appendChild(newNota);
+		newNota.classList.add("newNota");
+
+		newNota.insertBefore(notaUno, newNota.childNodes[0]);
+		newNota.insertBefore(fondo, newNota.childNodes[1]);
+
+		notaUno.classList.add("inicial");
+		notaUno.style.display = "block";
+
+		
+	}
 });
 
 
